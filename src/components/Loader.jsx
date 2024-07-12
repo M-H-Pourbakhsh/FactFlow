a6d273ae6479341ae9cf9e3797b68a00df92e02c
Mohammad Hasan Pourbakhsh <mh.pourbakhsh@gmail.com>
2024-07-12 22:41:17 +0330

    adjusting the spinner image's path according to base path in vite.config.js

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <div className="spinner">
-      <img src={`${import.meta.env.BASE_URL}spinner.png`} alt="Loading..." />
+      <img src={`${import.meta.env.BASE_URL}spinner.svg`} alt="Loading..." />
     </div>
   )
 }
